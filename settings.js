4edd49656194e155814bd3941ed0d8fd94c21dd2
theknock <31511731+theknock@users.noreply.github.com>
2017-08-31 23:13:33 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://theknock-email-opt-in.herokuapp.com/confirmEmail';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "tribe@theknock.com";
+exports.senderName = "TheKnock";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
@@ -13,5 +13,5 @@ exports.listId = null;
 exports.templateId = null;
 
 // receive an email when a new signup is confirmed
-exports.sendNotification = true;
-exports.notificationEmail = "admin@example.com";
+exports.sendNotification = false;
+exports.notificationEmail = null;
